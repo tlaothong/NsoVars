3ee4214c6c495e4e76d465366a96bff7d998ae9e
Teerachai Laothong <teerachai@perfenterprise.com>
2018-09-20 07:58:35 +0700

    Add initial model and controller.

---
+++ VarsWebApi/Models/Building.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VarsWebApi.Models
+{
+    /// <summary>
+    /// อาคาร / สิ่งปลูกสร้าง
+    /// </summary>
+    public class Building
+    {
+    }
+}
---
+++ VarsWebApi/Models/HouseHoldSample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VarsWebApi.Models
+{
+    /// <summary>
+    /// ครัวเรือน - หน่วยตัวอย่าง
+    /// </summary>
+    public class HouseHoldSample
+    {
+        /// <summary>
+        /// ที่อยู่
+        /// </summary>
+        public string Address { get; set; }
+    }
+}
