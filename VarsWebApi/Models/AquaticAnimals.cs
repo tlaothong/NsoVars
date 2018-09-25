1fcb22bf91d2abff197376b6296e4354de4c12db
Nuttapont Tantibanchachai <nuttapont.t@kkumail.com>
2018-09-25 15:50:41 +0700

    Mick 2.6-2.9

--- VarsWebApi/Models/AquaticAnimals.cs
+++ VarsWebApi/Models/AquaticAnimals.cs
@@ -1,6 +1,48 @@
 namespace VarsWebApi.Models
 {
-    public class AquaticAnimals
+    public class AquaticAnimals : ModelBase
     {
+        public bool AquAnimal { get; set; }
+        /// <summary>
+        /// ในรอบ 12 เดือนที่ผ่านมาครัวเรือนนี้ท้าการเพาะเลี้ยง เพาะฟัก อนุบาลสัตว์น้้าในพื้นที่น้้าจืดหรือไม่ 
+        /// </summary>
+        public bool Fish { get; set; }
+        /// <summary>
+        /// ปลา
+        /// </summary>
+        public bool Shrimp { get; set; }
+        /// <summary>
+        /// กุ้ง
+        /// </summary>
+        public bool Frog { get; set; }
+        /// <summary>
+        /// กบ
+        /// </summary>
+        public bool Crocodile { get; set; }
+        /// <summary>
+        /// จระเข้
+        /// </summary>
+        public bool SnappingTurtle { get; set; }
+        /// <summary>
+        /// ตะพาบน้ำ
+        /// </summary>
+        public bool Crab { get; set; }
+        /// <summary>
+        /// ปู
+        /// </summary>
+        public bool ShellFish { get; set; }
+        /// <summary>
+        /// หอย
+        /// </summary>
+        public bool Turtle { get; set; }
+        /// <summary>
+        /// เต่า
+        /// </summary>
+        public bool Reddish { get; set; }
+        /// <summary>
+        /// ไรแดง
+        /// </summary>
+
+
     }
 }
