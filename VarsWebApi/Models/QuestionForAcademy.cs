6bda29e01e5bebd34be4602ec4d7281911a9fad3
Aphaphon <pui.aphaphon@gmail.com>
2018-09-26 11:54:07 +0700

    Update Academy

--- VarsWebApi/Models/QuestionForAcademy.cs
+++ VarsWebApi/Models/QuestionForAcademy.cs
@@ -10,6 +10,11 @@ namespace VarsWebApi.Models
         /// ก่อนวัยเรียน 
         /// </summary>
         public bool PreSchool  { get; set; }
+
+        /// <summary>
+        /// ก่อนวัยเรียนกี่คน
+        /// </summary>
+        public int PreSchoolCount  { get; set; }
         
         /// <summary>
         /// อนุบาล  
@@ -17,9 +22,19 @@ namespace VarsWebApi.Models
         public bool Kindergarten  { get; set; }
 
         /// <summary>
+        /// อนุบาลกี่คน
+        /// </summary>
+         public int KindergartenCount  { get; set; }
+
+        /// <summary>
         /// ประถมศึกษา  
         /// </summary>
         public bool PrimarySchool  { get; set; }
+
+        /// <summary>
+        /// ประถมศึกษากี่คน
+        /// </summary>
+         public int PrimarySchoolCount  { get; set; }
 
         /// <summary>
         /// มัธยมศึกษา  
@@ -27,14 +42,29 @@ namespace VarsWebApi.Models
         public bool Highschool  { get; set; }
 
         /// <summary>
+        /// มัธยมศึกษากี่คน
+        /// </summary>
+         public int HighschoolCount  { get; set; }
+
+        /// <summary>
         /// ปวช./ปวส 
         /// </summary>
         public bool Vocational  { get; set; }
 
         /// <summary>
+        /// ปวช./ปวส กี่คน
+        /// </summary>
+         public int VocationalCount  { get; set; }
+
+        /// <summary>
         /// อุดมศึกษา  
         /// </summary>
         public bool HigherEducation  { get; set; }
+
+        /// <summary>
+        /// อุดมศึกษากี่คน
+        /// </summary>
+         public int HigherEducationCount  { get; set; }
 
         /// <summary>
         /// จำนวนบุคลากรรวมทั้งหมดทุกประเภท
