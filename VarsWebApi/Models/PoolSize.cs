d439999b9b7ee88917c8cc4c036ab2c429e732e1
powerofwind <powerofwind10@gmail.com>
2018-09-26 14:30:23 +0700

    edit RectanglePool

--- VarsWebApi/Models/PoolSize.cs
+++ VarsWebApi/Models/PoolSize.cs
@@ -12,7 +12,7 @@ namespace VarsWebApi.Models
         /// <summary>
         /// ถ้าสระเป็นสี่เหลี่ยม กว้าง ยาว ลึกเท่าไร
         /// </summary>
-        public SquarePool PoolSquare { get; set; }
+        public RectanglePool PoolSquare { get; set; }
         /// <summary>
         /// ถ้าสระเป็นวงกลม เส้นผ่านศูนย์กลางกี่เมตร ลึกเท่าไร
         /// </summary>
