51cb3ec270e69f4645f5b827f9e87c3c1bea7cf7
MekaMask <agapevb35@gmail.com>
2018-09-27 16:03:22 +0700

    Edit Disasterous.cs and add YearsDisastersous.cs move to folder S06

---
+++ VarsWebApi/Models/S06/Disasterous.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VarsWebApi.Models
+{
+    /// <summary>
+    /// ปัญหาอุทกภัย
+    /// </summary>
+    // TODO: go
+    public class Disasterous:ModelBase
+    {
+        /// <summary>
+        /// ในรอบ 5 ปีที่ผ่านมา (2556-2560) ที่อยู่อาศัยนี้ประสบปัญหาอุทกภัย (น้ำท่วมขัง น้ำรอการระบาย) หรือไม่
+        /// </summary>
+        public bool Flooded { get; set; }
+
+        /// <summary>
+        /// มีปัญหาอุทกภัยรอบ 5 ปีที่ผ่านมา (2556-2560)
+        /// </summary>
+        public List<YearsDisasterous> YearsDisasterous { get; set; }
+    }
+}
