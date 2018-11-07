ebfb8bc265544d77a401f9922e6fb29edba809c9
aphaphon <pui.aphaphon@gmail.com>
2018-11-07 12:20:50 +0700

    pumpBase

--- VarsWebApi/Controllers/SurveyController.cs
+++ VarsWebApi/Controllers/SurveyController.cs
@@ -950,8 +950,10 @@ namespace VarsWebApi.Controllers
                         {
                             Doing = true,
                             WaterResourceCount = 1,
-                            WaterResources = new List<WaterConsumptionUsingPumpBase>(){
-                                new WaterConsumptionUsingPumpBase(){
+                            WaterResources = new List<WaterConsumptionUsingPump>(){
+                                new WaterConsumptionUsingPump(){
+                                    HasCubicMeterPerMonth = true,
+                                    CubicMeterPerMonth = 5,                                    
                                     HasPump = true,
                                     PumpCount = 1,
                                     Pumps = new List<Pump>(){
