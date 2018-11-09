53e008049be0c9578349e0cdb0c3ef405b06c4d5
powerofwind <powerofwind10@gmail.com>
2018-11-09 13:05:53 +0700

    edit Suctionpipe

--- VarsWebApi/Models/shared/SuctionPipeSize.cs
+++ VarsWebApi/Models/shared/SuctionPipeSize.cs
@@ -13,194 +13,102 @@ namespace VarsWebApi.Models
         /// <summary>
         /// (1/2)-1
         /// </summary>
-        /// <remarks>
-        /// น้ำผิวดิน - ไฟฟ้า - ปั๊มน้ำอัตโนมัติ ถังกลม/ ถังเหลี่ยม
-        /// </remarks>
-        Num1 = 1,
-        // /// <summary>
-        // /// 1(1/2)-2
-        // /// </summary>
-        // Num1 = 1,
-        
-        //  /// <summary>
-        // /// (1/2)-2
-        // /// </summary>
-        // Num1 = 1,
-
-
-        // //////////////////
-        //  /// <summary>
-        // /// (1/2)-2
-        // /// </summary>
-        // /// <remarks>
-        // /// น้ำผิวดิน - ไฟฟ้า - ปั๊มหอยโข่ง
-        // /// </remarks>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// (1/2)-1 (1/2)
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 2
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 3
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 4
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// (1/2)-3
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 5 - 6
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// (1/2)-6
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 7 - 8
-        // /// </summary>
-        // Num1 = 1,
-
-        //  //////////////////
-        //  /// <summary>
-        // /// (1/2)-2
-        // /// </summary>
-        // /// <remarks>
-        // /// น้ำผิวดิน - น้ำมันดีเซล - ปั๊มหอยโข่ง/ปั๊มพญานาค
-        // /// </remarks>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 3
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 4
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 5  - 6
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// (1/2)-3
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 7 - 8
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 4 - 6
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// (1/2)-4
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 9 - 10
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// (1/2)-6
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 11 - 12
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// (1/2)-8
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// (1/2)-10
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 6 - 12
-        // /// </summary>
-        // Num1 = 1,
-
-        // //////////////////
-        //  /// <summary>
-        // /// (1/2)-2
-        // /// </summary>
-        // /// <remarks>
-        // /// น้ำผิวดิน - น้ำมันเบนซิน - ปั๊มหอยโข่ง/ปั๊มพญานาค
-        // /// </remarks>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 3
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 4
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 5  - 6
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// (1/2)-4
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 7 - 8
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 9-10
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// (1/2)-6
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 11 - 12
-        // /// </summary>
-        // Num1 = 1,
-
-        // //////////////////
-        // /// <summary>
-        // /// (1/2)-2
-        // /// </summary>
-        // /// <remarks>
-        // /// น้ำผิวดิน - รถไถเดินตาม - ปั๊มหอยโข่ง/ปั๊มพญานาค
-        // /// </remarks>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 3
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 4
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 5  - 6
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 7 - 8
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 9 - 10
-        // /// </summary>
-        // Num1 = 1,
-        //  /// <summary>
-        // /// 11 - 12
-        // /// </summary>
-        // Num1 = 1,
-        
-        
-        
+        SP12_1 = 1,
+        /// <summary>
+        /// (1/2)-2
+        /// </summary>
+        SP12_2 = 1,
+        /// <summary>
+        /// (1/2)-3
+        /// </summary>
+        SP12_3 = 1,
+        /// <summary>
+        /// (1/2)-4
+        /// </summary>
+        SP12_4 = 1,
+        /// <summary>
+        /// (1/2)-6
+        /// </summary>
+        SP12_6 = 1,
+        /// <summary>
+        /// (1/2)-8
+        /// </summary>
+        SP12_8 = 1,
+        /// <summary>
+        /// (1/2)-10
+        /// </summary>
+        SP12_10 = 1,
+        /// <summary>
+        /// (1/2)-1 (1/2)
+        /// </summary>
+        SP12_112 = 1,
+        /// <summary>
+        /// 1(1/2)-2
+        /// </summary>
+        SP112_2 = 1,
+        /// <summary>
+        /// 1(1/4)-2
+        /// </summary>
+        SP114_2 = 1,
+        /// <summary>
+        /// 1
+        /// </summary>
+        SP1 = 1,
+        /// <summary>
+        /// 2
+        /// </summary>
+        SP2 = 1,
+        /// <summary>
+        /// 2-3
+        /// </summary>
+        SP2_3 = 1,
+        /// <summary>
+        /// 2-4
+        /// </summary>
+        SP2_4 = 1,
+        /// <summary>
+        /// 3
+        /// </summary>
+        SP3 = 1,
+        /// <summary>
+        /// 4
+        /// </summary>
+        SP4 = 1,
+        /// <summary>
+        /// 4 - 6
+        /// </summary>
+        SP4_6 = 1,
+        /// <summary>
+        /// 4 - 8
+        /// </summary>
+        SP4_8 = 1,
+        /// <summary>
+        /// 5
+        /// </summary>
+        SP5 = 1,
+        /// <summary>
+        /// 5 - 6
+        /// </summary>
+        SP5_6 = 1,
+        /// <summary>
+        /// 6
+        /// </summary>
+        SP6 = 1,
+        /// <summary>
+        /// 6 - 12
+        /// </summary>
+        SP6_12 = 1,
+        /// <summary>
+        /// 7 - 8
+        /// </summary>
+        SP7_8 = 1,
+        /// <summary>
+        /// 9 - 10
+        /// </summary>
+        SP9_10 = 1,
+        /// <summary>
+        /// 11 - 12
+        /// </summary>
+        SP11_12 = 1,
     }
 }
