7f05f39e56b0858668a4251e00b01760bad6ee9b
SorrY <so.sikarin@perfenterprise.com>
2018-12-18 10:49:59 +0700

    edit name

--- VarsWebApi/Models/Demo/Work.cs
+++ VarsWebApi/Models/Demo/Work.cs
@@ -10,7 +10,7 @@ namespace VarsWebApi.Models
     {
         [BsonId]
         public string IdEA { get; set; }
-        public string UserId { get; set; }
+        public string IdUser { get; set; }
         public string Province { get; set; }
         public string District { get; set; }
         public string SubDistrict { get; set; }
