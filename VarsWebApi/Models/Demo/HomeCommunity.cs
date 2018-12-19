860d54a6066ccef0eb914f9e131f2a6a6019f669
lonelycay99 <nerddy.hahaha@hotmail.com>
2018-12-19 10:30:05 +0700

    Implement Controller

--- VarsWebApi/Models/Demo/HomeCommunity.cs
+++ VarsWebApi/Models/Demo/HomeCommunity.cs
@@ -7,6 +7,7 @@ namespace VarsWebApi.Models
 {
     public class HomeCommunity
     {
+        public string IdEA { get; set; }
         public string address { get; set; }
         public string name { get; set; }
         public string status { get; set; }
